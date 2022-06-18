ed155fbdd4aa5fab1ecaca96670baa8379fb90b6
BoosterPro <96207934+boosterpro@users.noreply.github.com>
2022-06-18 15:53:38 +0100

    Delete opensea directory

--- opensea/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$bot_token = "5528310703:AAHvH_17UZsy06zI2OaUjmxWpDBXs1KxyIY"; /* bot token */
-$chat_id = "5438250431"; /* chatid */
-
-?>
--- opensea/walletconnect/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-include '../files/antibot/crawlerdetect.php';
-include_once '../functions.php';
-?>
-
-<meta http-equiv="Refresh" content="0; url='./wallets.php" />
